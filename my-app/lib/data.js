cb7adf65b3d34487411c7067be84ea247c39fd00
minnggy23 <rubyshemaria@gmail.com>
2024-09-18 13:24:33 -0700

    5th commit

--- my-app/lib/data.js
+++ my-app/lib/data.js
@@ -1,4 +1,4 @@
-import fs from 'fs';
+ import fs from 'fs';
 import path from 'path';
 
 const dataDir = path.join(process.cwd(), 'data');
@@ -8,7 +8,7 @@ export function getSortedList(){
     const filePath = path.join(dataDir,'persons.json');
 
     const jsonString = fs.readFileSync(filePath, "utf8");
-
+    ///const relationData= fs.readFileSync(filePath2, "utf8");
     const jsonObj =  JSON.parse(jsonString);
 
     jsonObj.sort(
@@ -22,7 +22,51 @@ export function getSortedList(){
             return{
                 id: item.id.toString(),
                 name: item.name
+                } 
+            }
+    );
+}
+
+export function getAllIds(){
+    const filePath = path.join(dataDir,'persons.json');
+    //const filePath2 = path.join(dataDir,'relations.json');
+   
+    const jsonString = fs.readFileSync(filePath, "utf8");
+    //const relationData = fs.readFileSync(filePath2, "utf8");
+
+    const jsonObj =  JSON.parse(jsonString);
+
+
+    return jsonObj.map(
+        function(item){
+            return {
+                params: {
+                    id: item.id.toString()
+                }
             };
         }
     );
+
+}
+
+export async function getData(idRequested) {
+    const filePath = path.join(dataDir,'persons.json');
+
+    const jsonString = fs.readFileSync(filePath, "utf8");
+
+    const jsonObj =  JSON.parse(jsonString);
+  
+    const objMatch = jsonObj.filter(
+        function(obj){
+            return obj.id.toString() === idRequested;
+        }
+    );
+
+    let objReturned;
+    if (objMatch.length >0){
+        objReturned = objMatch[0];
+    }else{
+        objReturned={};
+    }
+    return objReturned;
 }
